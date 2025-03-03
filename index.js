42810e171ff5ae5563a8616379e8ab007e102cc1
Temur Coder <temurnarzullayev810@gmail.com>
2025-03-03 15:24:20 +0300

    added

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 class Account {
-    constructor(owner,currency,pin){
-        this.owner = owner;
-        this.currency = currency;
-        this.pin = pin;
-        this.balance = 1000;
+  constructor(owner, currency, pin, balance) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.balance = balance;
+    this.movements = [];
+  }
+  deposit(amount) {
+    this.amount = amount;
+  }
+  withdraw(amount) {
+    if (amount > this.currency) {
+      console.log("You have not enough money");
+    } else {
+      this.currency -= amount;
     }
-    deposit(amount){
-        this.amount = amount;
-    }
-    withdraw(amount){
-        if (amount>this.currency){
-            console.log("You have not enough money");
-        } else {
-            this.currency -= amount;
-        }
-    }
+  }
 }
-const account = new Account('John', 'USD', 1111)
+const account = new Account("Temurbek", "USD", 1111, 1000);
 console.log(account);
